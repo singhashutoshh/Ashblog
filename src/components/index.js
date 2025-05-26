40861b6e5e21d7e0930e58caa28826bc44d7cb0d
singhashutoshh <ashutoshsingh9354@gmail.com>
2025-05-26 15:09:04 +0530

    final bug update

--- src/components/index.js
+++ src/components/index.js
@@ -10,7 +10,7 @@ import Login from "./Login";
 import Button from "./Button";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
-import AuthLayout from "./AuthLayout";
+import AuthLayout from "./Authlayout";
 import Input from "./Input";
 export {
     Header,
